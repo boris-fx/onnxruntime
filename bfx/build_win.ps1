702d248c79d4d5fa8a5618a38913948ae72536da
Carson Swope <carsonmswope@gmail.com>
2026-08-11 08:12:36 -0600

    disable parallel build on windows to avoid OOM in flash attention

--- bfx/build_win.ps1
+++ bfx/build_win.ps1
@@ -85,7 +85,7 @@ Pop-Location
 $CMAKE_CUDA_FLAGS = "-static-global-template-stub=false" # this needed for compilation when switching to CU 12.8 from 12.4
 $CMAKE_CUDA_ARCHITECTURES = "60-real;61-real;70-real;75-real;80-real;86-real;89-real;90a-real;90-real;90-virtual;120-real;120-virtual"
 
-$COMMON_BUILD_ARGS = "python tools\ci_build\build.py --config ${BUILD_CONFIG} --build_shared_lib --parallel 4 --use_dml --skip_tests --use_webgpu shared_lib"
+$COMMON_BUILD_ARGS = "python tools\ci_build\build.py --config ${BUILD_CONFIG} --build_shared_lib --parallel 1 --use_dml --skip_tests --use_webgpu shared_lib"
 $COMMON_BUILD_DIR = "$(Get-Location)\build"
 
 $DIST_DIR="$(Get-Location)\build\${DIST_NAME}"
@@ -98,7 +98,7 @@ $X86_64_NAME = "x86_64"
 $X86_64_BUILD_DIR = "${COMMON_BUILD_DIR}\${X86_64_NAME}"
 $X86_64_DIST_LIB_DIR="${DIST_LIB_DIR}\${X86_64_NAME}"
 $X86_64_CMAKE_EXTRA_DEFINES = "CMAKE_CUDA_FLAGS=${CMAKE_CUDA_FLAGS} CMAKE_CUDA_ARCHITECTURES=${CMAKE_CUDA_ARCHITECTURES} onnxruntime_BUILD_UNIT_TESTS=OFF onnxruntime_USE_FLASH_ATTENTION:BOOL=ON"
-$X86_64_ARGS = "--cmake_generator Ninja --use_cuda --cuda_home ${CUDA_HOME} --cudnn_home ${CUDNN_HOME} --cmake_extra_defines ${X86_64_CMAKE_EXTRA_DEFINES}"
+$X86_64_ARGS = "--cmake_generator Ninja --use_cuda --cuda_home ${CUDA_HOME} --cudnn_home ${CUDNN_HOME} --nvcc_threads 1 --flash_nvcc_threads 1 --cmake_extra_defines ${X86_64_CMAKE_EXTRA_DEFINES}"
 $X86_64_BUILD_CMD = "${COMMON_BUILD_ARGS} --build_dir ${X86_64_BUILD_DIR}  ${X86_64_ARGS}"
 
 $ARM64_NAME = "arm64"
